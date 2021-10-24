09dc9975b2137d5a243aa75bc770edc3a824b9d6
Zz <oliani748@gmail.com>
2021-10-24 07:50:03 -0300

    Create FibonacciAgain.java

---
+++ Mathematics/FibonacciAgain.java
@@ -0,0 +1,37 @@
+/*
+Here is the Problem: https://www.urionlinejudge.com.br/judge/en/problems/view/1531
+*/
+import java.util.Scanner;
+
+public class FibonacciAgain {
+    public static void main(String[] args) {
+        
+        Scanner reader = new Scanner(System.in);
+
+        while(reader.hasNextLine()){
+            int n = reader.nextInt();
+            int m = reader.nextInt();
+
+            // Calculate Fib(n)
+            n = CalcularFibonacci(n);
+
+            // Calculate Fib(Fib(n)) and divide by m
+            System.out.println(CalcularFibonacci(n)/m);
+        }
+        reader.close();
+    }
+
+    public static int CalcularFibonacci(int n){
+
+            int fib1 = 1;
+            int fib2 = 0;
+            int inter = 0;
+
+            for (int i = 1; i < n; i++){
+                inter = fib1;
+                fib1 += fib2;
+                fib2 = inter;
+            }
+        return fib1;
+    }
+}
