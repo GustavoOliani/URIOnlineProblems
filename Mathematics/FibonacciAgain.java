dd787f615e1a04a25e1c5519bed1df249991c273
Zz <oliani748@gmail.com>
2021-10-24 19:49:37 -0300

    Update FibonacciAgain.java

--- Mathematics/FibonacciAgain.java
+++ Mathematics/FibonacciAgain.java
@@ -8,7 +8,7 @@ public class FibonacciAgain {
     public static void main(String[] args) {
         
         Scanner reader = new Scanner(System.in);
-
+        
         while(reader.hasNextLine()){
             int n = reader.nextInt();
             int m = reader.nextInt();
@@ -16,23 +16,21 @@ public class FibonacciAgain {
             // Calculate Fib(n)
             n = CalcularFibonacci(n);
 
-            // Calculate Fib(Fib(n)) and divide by m
-            System.out.println(CalcularFibonacci(n)/m);
+            // Calculate the remainder of Fib(Fib(n)) divided by m
+            System.out.println(CalcularFibonacci(n) % m);
         }
         reader.close();
     }
 
     public static int CalcularFibonacci(int n){
 
-            int fib1 = 1;
-            int fib2 = 0;
-            int inter = 0;
+        int fib1 = 1, fib2 = 0, inter = 0;
 
-            for (int i = 1; i < n; i++){
-                inter = fib1;
-                fib1 += fib2;
-                fib2 = inter;
-            }
+        for (int i = 1; i < n; i++){
+            inter = fib1;
+            fib1 += fib2;
+            fib2 = inter;
+        }
         return fib1;
     }
 }
