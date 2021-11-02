c414a04ea6a4058ca2fdb63a8948de9f019b44d3
Zz <oliani748@gmail.com>
2021-11-02 02:08:34 -0300

    Update FastPrimeNumber.java

--- Mathematics/FastPrimeNumber.java
+++ Mathematics/FastPrimeNumber.java
@@ -15,11 +15,11 @@ public class FastPrimeNumber {
     }
     public static void TestForPrime(long testNumber){
         String answer = "Prime";
-        long squareRootTestNumber = (long) Math.sqrt(testNumber) + 1;
+        long squareRootTestNumber = (long) Math.sqrt(testNumber);
         if (testNumber % 2 == 0 && testNumber != 2){
             answer = "Not Prime";
         }else{
-            for (long i = 3; i < squareRootTestNumber; i+=2){
+            for (long i = 3; i <= squareRootTestNumber; i+=2){
                 if (testNumber % i == 0){
                     answer = "Not Prime";
                     break;
